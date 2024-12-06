6394228e2165f42754606c4516f7366820c66a2b
Paweł Bielecki <166497685+oksent-dev@users.noreply.github.com>
2024-12-06 18:35:00 +0100

    Create inorderTraversal.js

---
+++ solutions/inorderTraversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const result = [];
+  function inorder(node) {
+    if (!node) return;
+    inorder(node.left);
+    result.push(node.val);
+    inorder(node.right);
+  }
+  inorder(root);
+  return result;
+};
+
+//  Test cases
+import TreeNode from "../utils/treeNode.js";
+const tree = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
+console.log(inorderTraversal(tree)); // [1,3,2]
+console.log(inorderTraversal(null)); // []
+console.log(inorderTraversal(new TreeNode(1))); // [1]
