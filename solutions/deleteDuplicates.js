df3e92d27a834a53245641cd52960e187bd05b10
Paweł Bielecki <pbielecki1024@gmail.com>
2024-12-09 12:53:52 +0100

    Create deleteDuplicates.js

---
+++ solutions/deleteDuplicates.js
@@ -0,0 +1,41 @@
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
+Return the linked list sorted as well.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let current = head;
+  while (current !== null && current.next !== null) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
+
+// Test cases
+import ListNode from "../utils/ListNode.js";
+
+const head = new ListNode(1);
+head.next = new ListNode(1);
+head.next.next = new ListNode(2);
+deleteDuplicates(head); // 1 -> 2
+let current = head;
+while (current !== null) {
+  console.log(current.val);
+  current = current.next;
+}
