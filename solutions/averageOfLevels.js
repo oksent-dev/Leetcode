27b2058b9be49c47c040963b9300183055572f8f
Paweł Bielecki <pbielecki1024@gmail.com>
2024-12-16 10:41:03 +0100

    Create averageOfLevels.js

---
+++ solutions/averageOfLevels.js
@@ -0,0 +1,42 @@
+/*
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. 
+Answers within 10-5 of the actual answer will be accepted.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    let sum = 0;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      sum += node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(sum / size);
+  }
+  return result;
+};
+
+// Test cases
+import TreeNode from "../utils/treeNode.js";
+let root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+console.log(averageOfLevels(root)); // [3, 14.5, 11]
