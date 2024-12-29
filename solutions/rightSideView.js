c69a225d92caf409bd4223a888c164d166c26d29
Paweł Bielecki <pbielecki1024@gmail.com>
2024-12-29 15:15:29 +0100

    righSideView and uniquePaths

---
+++ solutions/rightSideView.js
@@ -0,0 +1,41 @@
+/*
+Given the root of a binary tree, imagine yourself standing on the right side of it, 
+return the values of the nodes you can see ordered from top to bottom.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  const result = [];
+  if (!root) return result;
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (i === size - 1) result.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return result;
+};
+
+// Test cases
+import TreeNode from "../utils/treeNode.js";
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.left.right = new TreeNode(5);
+root.right = new TreeNode(3);
+root.right.right = new TreeNode(4);
+console.log(rightSideView(root)); // [1, 3, 4]
