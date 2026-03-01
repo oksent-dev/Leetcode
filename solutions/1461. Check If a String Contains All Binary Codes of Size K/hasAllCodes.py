69bdc9ee4a0fb47e0497f7b803ad87e03d4884df
Paweł Bielecki <166497685+oksent-dev@users.noreply.github.com>
2026-03-01 20:59:51 +0100

    Create hasAllCodes.py

---
+++ solutions/1461. Check If a String Contains All Binary Codes of Size K/hasAllCodes.py
@@ -0,0 +1,24 @@
+"""
+Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. 
+Otherwise, return false.
+"""
+from typing import List
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        return len(set(s[i:i+k] for i in range(len(s) - k + 1))) == 2**k
+
+# Test cases:
+if __name__ == "__main__":
+    solution = Solution()
+    s1 = "00110110"
+    k1 = 2
+    print(solution.hasAllCodes(s1, k1))  # Output: true
+
+    s2 = "0110"
+    k2 = 1
+    print(solution.hasAllCodes(s2, k2))  # Output: true
+
+    s3 = "0110"
+    k3 = 2
+    print(solution.hasAllCodes(s3, k3))  # Output: false
